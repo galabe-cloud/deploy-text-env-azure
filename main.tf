c10325049268c3b0a5c2d8e7becfa39d04936e01
Gabila_teck <79769753+galabe-cloud@users.noreply.github.com>
2023-04-17 07:29:10 -0700

    Create main.tf
    added main.tf

---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "azurerm" {
+features {}
+}
+
+resource "azurerm_resource_group" "dev_rg" {
+name     = "dev-rg"
+location = "eastus"
+}
+
+resource "azurerm_virtual_network" "dev_vnet" {
+name                = "dev-vnet"
+address_space       = ["10.0.0.0/16"]
+location            = azurerm_resource_group.dev_rg.location
+resource_group_name = azurerm_resource_group.dev_rg.name
+
+subnet {
+name           = "dev-subnet"
+address_prefix = "10.0.1.0/24"
+}
+}
+
+resource "azurerm_network_security_group" "dev_nsg" {
+name                = "dev-nsg"
+location            = azurerm_resource_group.dev_rg.location
+resource_group_name = azurerm_resource_group.dev_rg.name
+
+security_rule {
+name                       = "allow_ssh"
+priority                   = 1001
+direction                  = "Inbound"
+access                     = "Allow"
+protocol                   = "Tcp"
+source_port_range          = "*"
+destination_port_range     = "22"
+source_address_prefix      = "*"
+destination_address_prefix = "*"
+}
+
+security_rule {
+name                       = "allow_http"
+priority                   = 1002
+direction                  = "Inbound"
+access                     = "Allow"
+protocol                   = "Tcp"
+source_port_range          = "*"
+destination_port_range     = "80"
+source_address_prefix      = "*"
+destination_address_prefix = "*"
+}
+}
+
+resource "azurerm_linux_virtual_machine" "dev_vm" {
+name                = "dev-vm"
+location            = azurerm_resource_group.dev_rg.location
+resource_group_name = azurerm_resource_group.dev_rg.name
+size                = "Standard_B2s"
+admin_username      = "adminuser"
+network_interface_ids = [
+azurerm_network_interface.dev_nic.id
+]
+admin_ssh_key {
+username   = "adminuser"
+public_key = file("~/.ssh/id_rsa.pub")
+}
+os_disk {
+name              = "dev-osdisk"
+caching           = "ReadWrite"
+storage_account_type = "Premium_LRS"
+}
+source_image_reference {
+publisher = "Canonical"
+offer     = "UbuntuServer"
+sku       = "18.04-LTS"
+version   = "latest"
+}
+}
+
+resource "azurerm_network_interface" "dev_nic" {
+name                = "dev-nic"
+location            = azurerm_resource_group.dev_rg.location
+resource_group_name = azurerm_resource_group.dev_rg.name
+
+ip_configuration {
+name                          = "dev-nic-ipconfig"
+subnet_id                     = azurerm_subnet.dev_subnet.id
+private_ip_address_allocation = "Dynamic"
+public_ip_address_id          = azurerm_public_ip.dev_public_ip.id
+}
+}
+
+resource "azurerm_subnet" "dev_subnet" {
+name                  = "dev-subnet"
+resource_group_name   = azurerm_resource_group.dev
